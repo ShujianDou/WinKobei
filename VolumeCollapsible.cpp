ca1ce7ee00b3393dc825d074762edb195b4d4af4
Chay <visionistprojects@gmail.com>
2020-07-27 13:31:04 -0400

    Update VolumeCollapsible.cpp

--- VolumeCollapsible.cpp
+++ VolumeCollapsible.cpp
@@ -1,21 +1,21 @@
 #include "VolumeCollapsible.h"
 #include "Vpanel.h"
 
+// Generate a volumeCollapsible object via VTable Dict
 VolumeCollapsible::VolumeCollapsible(VTable^ volumeList)
 {
 	if (volumeList->Length() == 0) {
 		std::exception("Error occurred");
-		std::cout << "ER" << std::endl;
 	}
 	Volumes = volumeList;
 	CollapsibleControls = gcnew VTable(System::Windows::Forms::Panel::typeid);
 
 	this->AutoScroll = TRUE;
 	this->Visible = true;
-	std::cout << "ER" << std::endl; std::cout << "ER" << std::endl;
 	GenerateControls();
 }
 
+// Generate a volumeCollapsible object via standard .NET array.
 VolumeCollapsible::VolumeCollapsible(cli::array<Volume^>^ volumeList)
 {
 	if (volumeList->Length == 0)
@@ -65,28 +65,28 @@ void VolumeCollapsible::GenerateControls()
 		header->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &VolumeCollapsible::OnMouseClick);
 		header->OpenClose = FALSE;
 		header->Index = indexCount;
-		indexCount++;
+		indexCount += 2;
+		header->GenerateGraphics();
 		header->Location = System::Drawing::Point(hbufferW, hbufferH);
 		hbufferH += header->Height + 5;
-
 		list = gcnew System::Windows::Forms::Panel;
 		list->Width = this->Width;
-		list->Height = 300;
-		list->Location = System::Drawing::Point(0, header->Location.Y + 10);
-		indexCount++;
-		list->AutoScroll = FALSE;
+		list->Location = System::Drawing::Point(0, header->Location.Y + header->Height);
+		list->Height = 100;
+		/*list->AutoScroll = FALSE;
 		list->HorizontalScroll->Maximum = 0;
+		list->VerticalScroll->Maximum = 0;
 		list->BackColor = System::Drawing::Color::Red;
 		list->HorizontalScroll->Visible = FALSE;
+		list->VerticalScroll->Visible = FALSE;
 		//this->AutoScrollMinSize = System::Drawing::Size(0, 1000);
-		list->AutoScroll = TRUE;
+		list->AutoScroll = TRUE;*/
 		list->Visible = FALSE;
-		cbufferY = list->Location.Y;
+		//ShowScrollBar((HWND)list->Handle.ToInt32(), 3, FALSE); - Doesn't work
 		for (unsigned int i = 0; i < Vol->ChapterList->Length(); i++) {
 			VPanel^ chpCard = gcnew VPanel;
 			chpCard->VolChp = TRUE;
 			Chapter^ Chp = safe_cast<Chapter^>(Vol->ChapterList(i, TRUE));
-			std::cout << msclr::interop::marshal_as<std::string>(Chp->Text->ToString());
 
 			chpCard->Width = this->Width;
 			chpCard->BackColor = System::Drawing::Color::Orange;
@@ -94,22 +94,28 @@ void VolumeCollapsible::GenerateControls()
 			chpCard->Width = list->Width;
 			chpCard->VolChp = TRUE;
 			chpCard->Chp = Chp;
+			chpCard->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &VolumeCollapsible::OnPaint);
 			//chpCard->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &VolumeCollapsible::OnPaint);
-
 			chpCard->Location::set(System::Drawing::Point(0, cbufferY));
 			cbufferY += chpCard->Height + 5;
-
+			chpCard->GenerateGraphics();
 			list->Controls->Add(chpCard);
 		}
+		list->Height = (cbufferY);
+		cbufferY = 0;
 		this->Controls->Add(header);
 		this->Controls->Add(list);
 	}
 	//this->Controls->AddRange(gcnew cli::array<System::Windows::Forms::Control^>{header, list});
 }
 
-void VolumeCollapsible::AnimateClose()
+void VolumeCollapsible::AnimateClose(VPanel^& panel)
 {
-	throw gcnew System::NotImplementedException();
+	System::Windows::Forms::Panel^ chapterList = safe_cast<System::Windows::Forms::Panel^>(this->Controls[panel->Index + 1]);
+	//this->Controls[panel->Index + 2]->Visible = FALSE;
+	chapterList->Visible = FALSE;
+	for (int idx = this->Controls->Count - 1; idx > panel->Index + 1; idx--)
+		this->Controls[idx]->Location = System::Drawing::Point(0, this->Controls[idx]->Location.Y - chapterList->Height);
 }
 
 void VolumeCollapsible::AnimateOpen(VPanel^& panel)
@@ -117,6 +123,8 @@ void VolumeCollapsible::AnimateOpen(VPanel^& panel)
 	System::Windows::Forms::Panel^ chapterList = safe_cast<System::Windows::Forms::Panel^>(this->Controls[panel->Index + 1]);
 	//this->Controls[panel->Index + 2]->Visible = FALSE;
 	chapterList->Visible = TRUE;
+	for (int idx = panel->Index + 2; idx < this->Controls->Count; idx++)
+		this->Controls[idx]->Location = System::Drawing::Point(0, this->Controls[idx]->Location.Y + chapterList->Height);
 }
 
 void VolumeCollapsible::OnPaint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e)
@@ -124,37 +132,28 @@ void VolumeCollapsible::OnPaint(System::Object^ sender, System::Windows::Forms::
 	VPanel^ Vpan = safe_cast<VPanel^>(sender);
 	switch (Vpan->VolChp) {
 	case 0: {
-		System::Windows::Forms::Label^ headerTitle;
-		headerTitle = gcnew System::Windows::Forms::Label;
-		headerTitle->Visible = TRUE;
-		headerTitle->Text = Vpan->Vol->Name;
-		headerTitle->Width = Vpan->Width;
-		headerTitle->Height = Vpan->Height;
-		System::Drawing::Graphics^ graphics = headerTitle->CreateGraphics();
-		System::Drawing::Font^ tFont;
-		System::Drawing::SizeF Size;
-
-		int tw, th;
-		tw = headerTitle->DisplayRectangle.Width - 3;
-		th = headerTitle->DisplayRectangle.Height - 3;
-
-		for (int idx = 1; idx <= 100; idx++) {
-			tFont = gcnew System::Drawing::Font("Arial", (idx));
-			Size = graphics->MeasureString(Vpan->Vol->Name, tFont);
-			if ((Size.Width > tw) || (Size.Height > th)) {
-				headerTitle->Font = gcnew System::Drawing::Font("Arial", idx - 1);
-				break;
-			}
-		}
-
-		delete graphics;
 		System::Drawing::RectangleF rect = System::Drawing::RectangleF(0, 20, Vpan->Width, Vpan->Height);
-		graphics = e->Graphics;
+		System::Drawing::Graphics^ graphics = e->Graphics;
 		System::Drawing::StringFormat^ strfm = gcnew System::Drawing::StringFormat();
-		graphics->DrawString(headerTitle->Text, headerTitle->Font, gcnew System::Drawing::SolidBrush(System::Drawing::Color::Black), rect, strfm);
-		//delete Vpan;
-		delete headerTitle;
+		strfm->Trimming = System::Drawing::StringTrimming::None;
+		strfm->LineAlignment = System::Drawing::StringAlignment::Center;
+		strfm->FormatFlags = System::Drawing::StringFormatFlags::NoWrap | System::Drawing::StringFormatFlags::NoClip;
+		System::Console::WriteLine(Vpan->Vol->Name);
+		graphics->DrawString(Vpan->Vol->Name, Vpan->PFont, gcnew System::Drawing::SolidBrush(System::Drawing::Color::Black), rect, strfm);
+		delete graphics;
+		delete strfm;
 		break;
+	}
+	case 1: {
+		System::Drawing::RectangleF rect = System::Drawing::RectangleF(0, 20, Vpan->Width, Vpan->Height);
+		System::Drawing::Graphics^ graphics = e->Graphics;
+		System::Drawing::StringFormat^ strfm = gcnew System::Drawing::StringFormat();
+		strfm->Trimming = System::Drawing::StringTrimming::None;
+		strfm->LineAlignment = System::Drawing::StringAlignment::Center;
+		strfm->FormatFlags = System::Drawing::StringFormatFlags::NoWrap | System::Drawing::StringFormatFlags::NoClip;
+		graphics->DrawString(Vpan->Chp->Name, Vpan->PFont, gcnew System::Drawing::SolidBrush(System::Drawing::Color::Black), rect, strfm);
+		delete graphics;
+		delete strfm;
 	}
 	}
 }
@@ -169,5 +168,14 @@ void VolumeCollapsible::OnScroll(System::Object^ sender, System::Windows::Forms:
 void VolumeCollapsible::OnMouseClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
 {
 	VPanel^ vp = safe_cast<VPanel^>(sender);
-	AnimateOpen(vp);
+	switch (vp->OpenClose) {
+	case 0:
+		AnimateOpen(vp);
+		vp->OpenClose = TRUE;
+		break;
+	case 1:
+		vp->OpenClose = FALSE;
+		AnimateClose(vp);
+		break;
+	}
 }
