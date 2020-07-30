458b59645825f796a18703045a695dfc10d20442
Chay <visionistprojects@gmail.com>
2020-07-30 18:10:45 -0400

    Update BookCard.cpp

--- BookCard.cpp
+++ BookCard.cpp
@@ -4,6 +4,14 @@
 
 ref class BookCard : System::Windows::Forms::Panel {
 private:
+	System::Windows::Forms::ContextMenu^ cm;
+	System::Windows::Forms::PictureBox^ Cover;
+	System::Windows::Forms::Label^ Title;
+	System::Windows::Forms::Label^ Author;
+	System::Windows::Forms::Label^ Chapters;
+	System::Windows::Forms::Label^ ChaptersRead;
+	System::Windows::Forms::Label^ PercentageRead;
+
 	property bool openFlag2;
 public:
 
@@ -37,6 +45,24 @@ public:
 		}
 	};
 
+	delegate void OpenHandler(System::Object^ sender, System::EventArgs^ e);
+	Book^ AttachedBook;
+
+	event OpenHandler^ OnOpen;
+	event OpenHandler^ OnDelete;
+	event OpenHandler^ OnUpdateCLClick;
+	event OpenHandler^ OnUpdateNClick;
+
+	delegate void CardHandler(BookCard^ sender);
+	event CardHandler^ OnCardClick;
+
+	int bookIndex;
+	bool CoverOnly;
+
+	BookCard(Book^ book, bool coveronly);
+	~BookCard();
+
+	void OpenBook(System::Object^ sender, System::EventArgs^ e);
 	void DeleteBook(System::Object^ sender, System::EventArgs^ e);
 	void UpdateFromNetBook(System::Object^ sender, System::EventArgs^ e);
 	void UpdateChapterListBook(System::Object^ sender, System::EventArgs^ e);
